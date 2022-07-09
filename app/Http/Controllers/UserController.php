154f5f4925bbf778fb6e406c43b3bf4f52492e80
Luany Boneli <luany.boneli@hotmail.com>
2022-07-09 01:42:14 -0300

    Finalizada Trilha da Sprint 8

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = [
+            'nomes' => [
+                'Luany',
+                'Tom'
+            ]
+        ];
+        dd($users);
+    }
+
+    public function show($id)
+    {
+        dd('Id do usuário é '.$id);
+    }
+}

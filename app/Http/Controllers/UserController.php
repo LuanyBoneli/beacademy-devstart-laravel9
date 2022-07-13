7d1cb241a30d77dd3e5e71dbfaadcb35092cad7d
Luany Boneli <luany.boneli@hotmail.com>
2022-07-13 08:18:33 -0300

    Aula09 - Excluir Usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,4 +67,12 @@ class UserController extends Controller
         return redirect()->route("users.index");
     }
 
+    public function destroy($id)
+    {
+        if(!$user = $this->model->find($id))
+            return redirect()->route('users.index');
+        $user->delete();
+        return redirect()->route('users.index');
+    }
+
 }

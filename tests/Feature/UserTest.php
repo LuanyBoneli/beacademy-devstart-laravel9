912cae977accb659eb40c03d4df4aea79c58fa88
Luany Boneli <luany.boneli@hotmail.com>
2022-07-17 00:54:49 -0300

    Aula03 - Testes

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_user()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login',[
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get('/users');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create_user()
+    {
+        $response = $this->post('/login/create',[
+            'name' => 'Admin',
+            'email' => 'admin@master.com',
+            'password' => '1q2w3e4r',
+            'is_admin' => 1,
+        ]);
+        $response->assertStatus(200);
+    }
+}
